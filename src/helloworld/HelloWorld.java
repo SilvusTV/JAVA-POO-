40b3c2b3a063eec6a4be6400cc52d470f10c0039
SilvusTV <hugo.combe.84@gmail.com>
2024-03-14 10:29:59 +0100

    exit with q

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,12 +1,24 @@
 package helloworld;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-    }
-
-    public int somme(int a, int b) {
-        return a + b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World (Bonjour le monde");
+        System.out.println("Appuyer sur la touche q pour quitter");
+        String quit = sc.nextLine();
+        if ("q".equals(quit)) return;
+        if ("Q".equals(quit)) {
+            System.out.println("Appuyer la touche q et pas Q pour quitter");
+            sc.nextLine();
+        } else {
+            System.out.println("Appuyer sur Ia touche q pour quitter");
+            quit = sc.nextLine();
+            if ("q".equals(quit)) return;
+            else System.out.println("Dernier essai, appuyer sur Ia touche q pour quitter");
+            sc.nextLine();
+            System.out.println("Good bye");
+        }
     }
 }
