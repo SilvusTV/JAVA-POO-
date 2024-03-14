a80775d7e800cff00f2a5815986d187a4c2cc03e
SilvusTV <hugo.combe.84@gmail.com>
2024-03-14 09:28:48 +0100

    init project with helloWorld

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    public int somme(int a, int b) {
+        return a + b;
+    }
+}
